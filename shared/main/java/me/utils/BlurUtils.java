ccd11a57fdc95960292a11923c507441d4fff097
mc-zhonghuang <105552936+mc-zhonghuang@users.noreply.github.com>
2022-12-30 18:25:50 +0800

    Update BlurUtils.java

--- shared/main/java/me/utils/BlurUtils.java
+++ shared/main/java/me/utils/BlurUtils.java
@@ -3,7 +3,7 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/WYSI-Foundation/LiquidBouncePlus/
  *
- * This code belongs to WYSI-Foundation. Please give credits when using this in your repository.
+ * This code belongs to WYSI-Foundation. Please give credits when using this in your repository.a
  */
 package me.utils;
 
